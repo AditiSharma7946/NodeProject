179a09ebd7a963cb4535cba3610b4f589dcae34a
Aditi-Crowdstaffing <aditis@crowdstaffing.com>
2022-05-13 11:25:45 +0530

    made type assertions, functions, interfaces

--- src/typeBasics.js
+++ src/typeBasics.js
@@ -1,13 +1,12 @@
-var arra = [1, 2, 3, 4];
+var array = [1, 2, 3, 4];
 var arr = [1, true, 'Hello'];
 //tuple
 var coder = [1, 'Aditi', true];
 // Tuple Array
 var backend;
 backend = [
-    [1, 'Aditi'],
-    [2, 'Sakshi'],
-    [3, 'Govind'],
+    [1, 'ror', [1, 'Aditi', true]],
+    [2, 'nodeJS', [2, 'Sakshi', true]]
 ];
 console.log(backend);
 //Union hold then one type
@@ -21,3 +20,32 @@ var decisions;
     decisions[decisions["Right"] = 1] = "Right";
 })(decisions || (decisions = {}));
 console.log(decisions.Wrong);
+var user = {
+    id: 1,
+    name: 'Govind'
+};
+//Type Assertion tells compiler to treat an entity as diff type
+var uid = 1;
+// let userId = <number>uid
+var userId = uid;
+console.log(typeof (userId), userId);
+console.log(typeof (uid));
+//functions
+function addNum(a, b) {
+    return a + b;
+}
+console.log(addNum(2, 3));
+function log(message) {
+    console.log(message);
+}
+function sayHi() {
+    console.log('Hi!');
+}
+var message = sayHi();
+var coder1 = {
+    id: 1,
+    name: 'Govind'
+};
+var addi = function (x, y) { return x + y; };
+var sub = function (x, y) { return x - y; };
+console.log(addi(2, 2));
--- src/typeBasics.ts
+++ src/typeBasics.ts
@@ -36,3 +36,52 @@ type User = {
     id: 1,
     name: 'Govind',
   }
+
+//Type Assertion tells compiler to treat an entity as diff type
+  let uid: any = 1
+// let userId = <number>uid
+let userId = uid as number
+
+console.log(typeof(userId), userId);
+console.log(typeof(uid));
+
+//functions
+function addNum(a: number, b: number): number {
+    return a + b
+  }
+
+console.log(addNum(2,3));
+
+function log(message: string | number): void {
+console.log(message)
+}
+
+function sayHi(): void { 
+    console.log('Hi!')
+} 
+
+let message: any = sayHi(); 
+
+//interface 
+interface CoderInterface {
+    readonly id: number
+    name: string
+    tech?: string
+  }
+  
+  const coder1: CoderInterface = {
+    id: 1,
+    name: 'Govind',
+  }
+  
+  //can't be used with unions or primitives
+  
+  interface MathFunc {
+    (x: number, y: number): number
+  }
+  
+  const add: MathFunc = (x: number, y: number): number => x + y
+  const sub: MathFunc = (x: number, y: number): number => x - y
+
+  console.log(add(2,2));
+  
